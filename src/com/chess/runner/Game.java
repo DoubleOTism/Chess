de62772489a00a9040272e7e4f1f6a91b0dafd13
DoubleOTism <erich.pross.junior@gmail.com>
2022-03-21 09:06:34 +0100

    Šachy, základní verze
    Basic chess

---
+++ src/com/chess/runner/Game.java
@@ -0,0 +1,40 @@
+package com.chess.runner;
+
+import com.chess.board.Board;
+import com.chess.common.File;
+import com.chess.common.Location;
+import com.chess.piece.*;
+import com.chess.squares.Square;
+
+import java.util.Scanner;
+
+
+public class Game {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.printBoard();
+
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String line = scanner.nextLine();
+            String[] fromTo = line.split("->");
+            File fromFile = File.valueOf(String.valueOf(Character.toUpperCase(fromTo[0].charAt(0))));
+            int fromRank = Integer.parseInt(String.valueOf(fromTo[0].charAt(1)));
+            File toFile = File.valueOf(String.valueOf(Character.toUpperCase(fromTo[1].charAt(0))));
+            int toRank = Integer.parseInt(String.valueOf(fromTo[1].charAt(1)));
+
+            Square fromSq = board.getLocationSquareMap().get(new Location(fromFile, fromRank));
+            Square toSq = board.getLocationSquareMap().get(new Location(toFile, toRank));
+
+            fromSq.getCurrentPiece().makeMove(toSq);
+            fromSq.reset();
+
+            board.printBoard();
+        }
+
+    }
+
+    public static void printPiece(Moveable piece) {
+        piece.getValidMoves(null);
+    }
+}
